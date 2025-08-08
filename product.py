3cc5090465d6cf0317dff6f795df9370a59655bf
syyz-syyz <3204096838@qq.com>
2025-08-08 17:12:22 +0800

    Update product.py

--- product.py
+++ product.py
@@ -1,5 +1,5 @@
 import plotly.graph_objects as go
-
+import streamlit as st
 fig = go.Figure(go.Sankey(
     node=dict(label=["A", "B", "C"], color="rgb(200,200,200)"),
     link=dict(source=[0], target=[1], value=[1], color="rgb(150,150,150)")
